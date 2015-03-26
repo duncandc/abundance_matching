e2f051600ece0f17571e8f2279c7528212c914d9
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-03-26 15:11:49 -0400

    added script demonstrating how to use this package

--- example.py
+++ example.py
@@ -27,7 +27,7 @@ def main():
     f =  h5py.File(filepath+halo_catalogue+'.hdf5', 'r')
     HC = f.get(halo_catalogue)
     HC = np.array(HC)
-    #make cut to halo catalogue
+    #make completeness cut to halo catalogue
     mp_bolshoi = 1.35e8
     mpeak_cut = mp_bolshoi*100.0
     keep = (HC['Mpeak']>mpeak_cut)
@@ -37,17 +37,32 @@ def main():
     #calculate (sub-)halo mass function from halo catalogue
     halo_prop = 'Mpeak'
     x = HC[halo_prop]
-    bins = np.arange(10.2,16,0.1)
+    bins = np.arange(10.1,16,0.1)
+    p = dict(phi1=0.001,x1=14,alpha1=-1.0) #initial guess for parameters to fit function
     dndm_halo = fit_abundance(x, weights=1.0/Lbox**3.0, bins=bins, xlog=True,\
-                              fit_type='schechter', show_fit=True)
+                              fit_type='schechter', p = p, show_fit=True)
     
-    #define stellar mass function, Li and White 2009
-    dndm_gal = get_sdss_smf()
+    #define stellar mass function, Baldry 2008.
+    M0 = 10.648 + np.log10(0.7**2)
+    phi1 = 4.26 * 10**(-3) / 0.7**3
+    phi2 = 0.58 * 10**(-3) / 0.7**3
+    alpha1 = -0.46
+    alpha2 = -1.58
+    dndm_gal = cu.schechter_function.Log_Double_Schechter(M0,M0,phi1,phi2,alpha1,alpha2)
+    
+    #plot the stellar mass function
+    dm_star = np.arange(7,12,0.1)
+    fig = plt.figure(figsize=(3.3,3.3))
+    fig.subplots_adjust(left=0.2, right=0.85, bottom=0.2, top=0.9)
+    plt.plot(dm_star,dndm_gal(dm_star))
+    plt.yscale('log')
+    plt.ylim([10**(-8),1])
+    plt.show()
     
     #define the form of P(gal | halo)
     from scipy.stats import norm
     def sigma(y):
-        return 0.15 #fixed scatter model
+        return 0.2 #fixed scatter model
     def P_x(y, mu_xy, sigma=sigma):
         mu_x = mu_xy(y)
         sigma = sigma(y)
@@ -56,12 +71,13 @@ def main():
     
     #calculate mean of the relation
     #define samples of the galaxy abundance function
-    dm_star = np.arange(5,12,0.1)
+    dm_star = np.arange(0,12.5,0.01)
     dn_star = dndm_gal(dm_star)
     #define samples of the halo abundance function
-    dm_halo = np.arange(10,15,0.1)
+    dm_halo = np.arange(10.0,15,0.01)
     dn_halo = dndm_halo(dm_halo)
-    mu_xy = AM(dn_star, dm_star, dn_halo, dm_halo, P_x, y_min = np.amin(dm_halo), y_max = np.amax(dm_halo), ny=30)
+    mu_xy = AM(dn_star, dm_star, dn_halo, dm_halo,\
+               P_x, y_min = np.amin(dm_halo), y_max = np.amax(dm_halo), ny=30)
 
     #apply the mean
     P = lambda y: P_x(y, mu_xy=mu_xy)
@@ -79,27 +95,6 @@ def main():
     plt.ylim([9,12])
     plt.xlim([9,15])
     plt.show(block=True)
-
-
-def get_sdss_smf():
-    from scipy import interpolate
-    #define stellar mass function, Li and White, 2009
-    def dn_dmstar(m,dm):
-        alpha = -1.155
-        m_star = 10**10.525
-        phi_star = 0.0083
-        
-        dndm = np.exp(-m/m_star)*phi_star*(m/m_star)**alpha* dm/m_star
-        return dndm
-    logm = np.arange(5.0,12.5,0.1)
-    m = 10.0**logm
-    dlogm = logm[1:]-logm[:-1]
-    dm = m[1:]-m[:-1]
-    mstar = logm[:-1]
-    dnstar = dn_dmstar(m[:-1],dm)/dlogm
-    dndlogMstar = interpolate.interp1d(mstar,dnstar)
-    
-    return dndlogMstar
 
 
 if __name__ == '__main__':
